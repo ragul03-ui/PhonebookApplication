183171c6b56227dd751e020a6308e852457dbb1b
ragul03-ui <125953363+ragul03-ui@users.noreply.github.com>
2023-07-28 11:36:19 +0530

    Add files via upload

---
+++ DAY5/AppBar/AppBar.jsx
@@ -0,0 +1,18 @@
+import { AuthNav } from 'components/AuthNav/AuthNav';
+import { Navigation } from 'components/Navigation/Navigation';
+import { UserMenu } from 'components/UserMenu/UserMenu';
+import { useSelector } from 'react-redux';
+import { selectAuth } from 'redux/auth/selectors';
+import { StyledAppBar } from './AppBar.styled';
+
+export const AppBar = () => {
+  //   const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useSelector(selectAuth);
+
+  return (
+    <StyledAppBar>
+      <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    </StyledAppBar>
+  );
+};
---
+++ DAY5/AppBar/AppBar.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledAppBar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* margin-bottom: 16px; */
+  padding: 0 16px;
+  /* border-bottom: 1px solid #2a363b; */
+  border-bottom: 1px solid #ececec;
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06),
+    1px 4px 6px rgba(0, 0, 0, 0.16);
+    
+`;
