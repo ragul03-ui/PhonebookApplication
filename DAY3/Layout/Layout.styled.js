b89b2af432593a9b8c7b48be9ad07941e97ed5ee
ragul03-ui <125953363+ragul03-ui@users.noreply.github.com>
2023-07-28 11:31:37 +0530

    Add files via upload

---
+++ DAY3/Layout/Layout.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+  max-width: 960;
+  margin: 0 auto;
+  @media screen and (max-width: 479px) {
+    max-width: 100%;
+  }
+
+  /* padding: 0 16px; */
+`;
+
+export const StyledSection = styled.div`
+  /* padding: 0 16px; */
+`;
